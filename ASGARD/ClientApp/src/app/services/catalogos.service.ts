70d33689a8b6a16ed3e90f22317f2262bb320891
KevinJovel <kevinjovel9@gmail.com>
2020-05-01 12:31:41 -0600

    otra prueba

--- ASGARD/ClientApp/src/app/services/catalogos.service.ts
+++ ASGARD/ClientApp/src/app/services/catalogos.service.ts
@@ -8,32 +8,21 @@ export class CatalogosService {
     constructor(private http: Http, @Inject("BASE_URL") baseUrl: string) {
         this.urlBase = baseUrl;
     }
-    //Sugiero que aqui trabajemos todos los catalogos solo que llevemos un orden con un comentario por cada uno
     //Servicios para marcas
-
     public getMarcas() {
         return this.http.get(this.urlBase + "api/Marcas/listarMarcas").map(res => res.json());
     }
-
     //Servicios Sucursales
-
     public getSucursales() {
         return this.http.get(this.urlBase + "api/Sucursal/listarSucursales").map(res => res.json());
     }
-
-    //Servicios Empleados
-
-    //Servicios Cargos
-
   //Service Donantes
   public agregarDonante(donante) {
     return this.http.post(this.urlBase + "api/Donantes/guardarDonante", donante).map(res => res.json());
   }
-
   public getDonantes() {
     return this.http.get(this.urlBase + "api/Donantes/listarDonantes").map(res => res.json());
   }
-
     //Servicios Proveeodres
     public getProveedores() {
         return this.http.get(this.urlBase + "api/Proveedor/listarProveedores").map(res => res.json());
