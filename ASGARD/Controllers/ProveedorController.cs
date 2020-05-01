65f324edb459402765fe16a30c52c5af687b70f9
Angie-Guzman <mbqg93@gmail.com>
2020-05-01 09:42:09 -0600

    guarda proveedor

--- ASGARD/Controllers/ProveedorController.cs
+++ ASGARD/Controllers/ProveedorController.cs
@@ -37,5 +37,41 @@ namespace ASGARD.Controllers
             }
         }
 
+
+        //Método guardar Proveedores
+        [HttpPost]
+        [Route("api/Proveedor/guardarProveedor")]
+        public int guardarProveedor([FromBody]ProveedoresAF oProveedoresAF)
+        {
+            int rpta = 0;
+
+            try
+            {
+                using (BDAcaassAFContext bd = new BDAcaassAFContext())
+                {
+                    Proveedor oProveedor = new Proveedor();
+                    oProveedor.IdProveedor = oProveedoresAF.idProveedor;
+                    oProveedor.Nombre = oProveedoresAF.Nombre;
+                    oProveedor.Direccion = oProveedoresAF.Direccion;
+                    oProveedor.Telefono = oProveedoresAF.Telefono;
+                    oProveedor.Rubro = oProveedoresAF.Rubro;
+                    oProveedor.Encargado = oProveedoresAF.Encargado;
+                    oProveedor.TelefonoEncargado= oProveedoresAF.TelefonoEncargado;
+                    oProveedor.Dhabilitado = 1;
+
+                    bd.Proveedor.Add(oProveedor);
+                    bd.SaveChanges();
+                    rpta = 1;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                rpta = 0;
+            }
+            return rpta;
+        }
+
+
     }
 }
